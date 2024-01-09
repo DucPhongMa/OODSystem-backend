80a98d47d022f4e3efa59df2f2ca2bfb055881ca
Vivy Vuong <vngvy48@gmail.com>
2024-01-08 22:11:05 -0500

    Installed pg, aws-S3 upload provider and updated the config files

---
+++ config/middlewares.js
@@ -0,0 +1,20 @@
+module.exports = [
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::poweredBy",
+
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*",
+      origin: ["http://localhost:1337", "http://localhost:3000"],
+    },
+  },
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
