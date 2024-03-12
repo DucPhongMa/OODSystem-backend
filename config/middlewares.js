426ce979882cb4772fbabd6c3eb5882db1fcd628
Vivy Vuong <vngvy48@gmail.com>
2024-03-11 21:00:00 -0400

    upgrade defaultLimit is 100 and update middlewares

--- config/middlewares.js
+++ config/middlewares.js
@@ -8,18 +8,24 @@ module.exports = [
     config: {
       enabled: true,
       headers: "*",
-      origin: ["http://localhost:1337", "http://localhost:3000", "https://ood-system-frontend.vercel.app"],
+      origin: [
+        "http://localhost:1337",
+        "http://localhost:3000",
+        "https://ood-system-frontend.vercel.app",
+        "http://prj666.ddns.net:1337",
+        "https://prj666.ddns.net:1337",
+      ],
     },
   },
   {
-    name: 'strapi::security',
+    name: "strapi::security",
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
           upgradeInsecureRequests: null,
         },
       },
